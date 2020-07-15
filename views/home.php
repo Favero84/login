be84a423ced468adf933d6a66dd7d6f4f675ec9a
Luiz Carlos Favero Junior <60441412+Favero84@users.noreply.github.com>
2020-07-15 19:28:38 -0300

    Add files via upload

---
+++ views/home.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Base Login</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="stylesheet" href="assets/css/bootstrap.min.css" />
+        <link href="<?php echo BASE_URL; ?>assets/css/style.css" rel="stylesheet" />
+        <script type="text/javascript" src="assets/js/jquery.min.js"></script>
+    </head>
+    <body>
+        Página Principal
+    </body>
+</html>
